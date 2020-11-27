dbb03f1ee273c63a5c2e1555c28d09febe8c2f57
Andrew <Andrew@Andrews-MBP.home>
2020-11-27 14:57:18 -0500

    add event handler functions and removed hidden css

--- index.js
+++ index.js
@@ -4,22 +4,82 @@ const spoonApiKey="ba1dd8bb59a545308760ceddf87a648c"
 // stretch goal* const youTubeURL="https://www.googleapis.com/youtube/v3/search"
 
 const STORE = {
-    food: [
-
-    ]
+    food: {},
+    movie: {},
+    started: false,
+    formFood: false,
+    formMovie: false,
 
 }
-// create start function to listen to button click
-function renderStart () {
+/*------- Render functions-----------------------*/
+
+// start the plan
+function renderStart() {
     $("main").html(`
     <section class="start">
     <h1>Plan the Perfect Quarantine Date Night</h1>
     <h4>Pick a recipe and movie for a pefect date night in!</h4>
     <button id="start">Start</button>
   </section>
+    `);
+}
+// render food form
+function renderFoodForm() {
+    $("main").html(`
+    <h2>Step 1</h2>
+    <h3>Let's start with finding a recipe to cook</h3>
+    <form id="js-form-food">
+            <label for="search-food">What type of food are you in the mood for?</label>
+            <input 
+            type="text"
+            name="search-food"
+            id="js-search-food"
+            required
+            placeholder="indian"
+            />
+            <input type="submit" value="Search Food" class="food"/>
+    </form>
     `)
 }
+// render movie form
+function renderMovieForm() {
+    $("main").html(`
+    <h2>Step 2</h2>
+    <h3>Next comes the entertainment</h3>
+    <form id="js-form-movie">
+    <label for="search-movie">What movie genre do you want to watch?</label>
+    <select name="search-movie" id="js-search-movie">
+        <option value="28">Action</option>
+        <option value="12">Adventure</option>
+        <option value="35">Comedy</option>
+        <option value="18">Drama</option>
+        <option value="27">Horror</option>
+        <option value="10749">Romance</option>
+        <option value="878">Sci Fi</option>
+        <option value="53">Thriller</option>
+    </select>
+    <input type="submit" value="Search Movie" class="movie">
+</form>
+    `)
+}
+// render results
+function renderResults() {
+    console.log("results will show here");
+}
 
+function render() {
+    if (!STORE.started) {
+        renderStart();
+    } else if (STORE.formFood & STORE.formMovie) {
+        renderResults();
+    } else if (!STORE.formFood & !STORE.formMovie) {
+        renderFoodForm();
+    } else if (STORE.formFood & !STORE.formMovie) {
+        renderMovieForm();
+    }
+}
+
+/*------- Watch forms-----------------------*/
 // create a watch form for food
 function watchFormFood() {
     $("#js-form-food").submit((event) => {
@@ -27,8 +87,8 @@ function watchFormFood() {
         const searchCity=$("#js-search-location").val();
         const searchFood=$("#js-search-food").val();
         // stretch goal* const searchMovie=$("js-search-movie").val();
-        console.log(searchFood)
-        getFood(searchFood)
+        console.log(searchFood);
+        getFood(searchFood);
     })
 }
 
@@ -38,21 +98,14 @@ function watchFormMovie() {
     $("#js-form-movie").submit((event)=>{
         event.preventDefault();
         const searchMovie= $("#js-search-movie :selected").val();
-        console.log(searchMovie)
+        console.log(searchMovie);
+        getMovie(searchMovie);
     })
 }
 watchFormMovie();
-//var myHeaders = new Headers();
-//myHeaders.append("Authorization", "Bearer Ginpac6lJcI7UvfB1IoF4zNjunhDCDm1FnoL3-1xmWe87RmzBXGvpanQaWEl98SHA_RGJy4tkA4kInGzxJ-p5eO-zsP4tYPhKqUr-fxGBG_IhFEfva0eUeJdclW9X3Yx");
 
-/*var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-};
-*/  
-
-
+/*------------------ GET functions ------------------------*/
+// get food
 function getFood(searchFood){
     const spoonURL=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonApiKey}&cuisine=${searchFood}`
     fetch(spoonURL)
@@ -67,10 +120,10 @@ function getFood(searchFood){
         $("#js-error-messageFood").text(`Something went wrong: ${err.message}`); 
     })
 }
-
-/*function getMovie(searchMovie){
-    const spoonURL=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonApiKey}&cuisine=${searchFood}&number=10`
-    fetch(spoonURL)
+// get movie
+function getMovie(searchMovie){
+    const movieURL=`https://api.themoviedb.org/3/discover/movie?api_key=${movieApiKey}&language=en-US&sort_by=popularity.desc&with_genres=${searchMovie}`
+    fetch(movieURL)
     .then((responseFood)=> {
         if (responseFood.ok){
             return responseFood.json();
@@ -82,30 +135,34 @@ function getFood(searchFood){
         $("#js-error-messageFood").text(`Something went wrong: ${err.message}`); 
     })
 }
-*/
-// GET function 
-/*function getDateNight(searchFood) {
-    const movieURL=`https://api.themoviedb.org/3/movie/popular?api_key=${movieApiKey}`
-    //const spoonURL=`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonApiKey}&cuisine=${searchFood}`
-    console.log(movieURL)
-    console.log(spoonURL)
-    Promise.all([
-        fetch(movieURL),
-        fetch(spoonURL)
-        ])
-        .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error(response.statusText);
-          })
-        .then((responseJson) => console.log(responseJson))
-        .catch((err) => {
-            $("#js-error-message").text(`Something went wrong: ${err.message}`);
-          });
-      }
+
+/*---------------- event handlers -------------------------*/
+function onStart() {
+    $("main").on('click', '#start', startedPlan);
+}
+
+function onFood() {
+    $("main").on('clicl', '')
+}
+
+function onRestart() {
+    $("main").on('click', '#restart', restartPlan);
+}
 
 
+function startedPlan() {
+    STORE.started = true;
+    render();
+}
+
+function restartPlan() {
+    STORE.started = false;
+    STORE.formFood = false;
+    STORE.formMovie = false;
+    STORE.food = {};
+    STORE.movie = {};
+    render();
+}
 
 // display results function 
 /*function displayResults(responseJson) {
@@ -113,4 +170,10 @@ function getFood(searchFood){
 }
 */
 
-// query params?
+function dateNight() {
+    onRestart();
+    onStart();
+    render();
+}
+
+$(dateNight);
