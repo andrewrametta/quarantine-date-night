41bc867b66f0dda9e14b2e29489600fd2d45f59c
Andrew <Andrew@Andrews-MBP.home>
2020-11-28 17:18:27 -0500

    add result render function and watch form

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const spoonApiKey="ba1dd8bb59a545308760ceddf87a648c"
 const STORE = {
     foodId: [],
     movieId: [],
-    foodTitle: "",
+    foodTitle: [],
     movieTitle: [],
     started: false,
     formFood: false,
-    formMovie: false
+    formMovie: false,
+    results: false,
 }
 /*------- Render functions-----------------------*/
 
@@ -45,6 +46,7 @@ function renderFoodForm() {
 }
 // render movie form
 function renderMovieForm() {
+    $('h3').empty();
     $("main").html(`
     <h2>Step 2</h2>
     <h3>Next comes the entertainment</h3>
@@ -60,13 +62,21 @@ function renderMovieForm() {
         <option value="878">Sci Fi</option>
         <option value="53">Thriller</option>
     </select>
-    <input type="submit" value="Search Movie" class="movie">
+    <input type="button" value="Search Movie" id="search-movie">
 </form>
     `)
 }
 // render results
 function renderResults() {
-    console.log("results will show here");
+    $('h3').empty();
+    $("main").html(`
+    <section class="show-results">
+    <h2>You picked ${STORE.foodTitle} and ${STORE.movieTitle}</h2>
+    <h4>If this is correct hit show results, if not hit restart</h4>
+    <button id="results">Show Results</button>
+    <button id="restart">Restart</button>
+  </section>
+    `);
 }
 
 function render() {
@@ -95,26 +105,50 @@ function watchFormFood() {
 watchFormFood();
 // create a watch form for movie 
 function watchFormMovie() {
-    $("#js-form-movie").click((event)=>{
+    $("main").on('click', '#search-movie', function(event){
         event.preventDefault();
-        const searchMovie= $("#js-search-movie :selected").val();
-        console.log(searchMovie);
+        console.log('hello')
+        const searchMovie= $("#js-search-movie").val()
         getMovie(searchMovie);
-    })
-}
+      });
+    }
 watchFormMovie();
 
+function watchResults() {
+    $('main').on("click", "#results", function(){
+        console.log('hello')
+        console.log(STORE.movieId)
+        console.log(STORE.foodId)
+        //getFoodResults(STORE.foodId)
+        //getMovieResult(STORE.movieId)
+    })
+}
+watchResults();
 /*------------------ watch results functions ---------------*/
 function watchFoodResults() {
     $("main").on("click", ".recipe-result", e=> {
-        STORE.foodId = Number($(e.currentTarget).attr('id'))
-        STORE.foodTitle = $(e.currentTarget).attr("alt");
+        STORE.foodId = Number($(e.currentTarget).attr('id'));
         console.log(STORE.foodId);
+        STORE.foodTitle = $(e.currentTarget).attr('alt')
         console.log(STORE.foodTitle);
+        $('h4').text(`You selected ${STORE.foodTitle}, if this is correct click the Next button, if not choose a differnt recipe`)
     });
 }
 
 watchFoodResults()
+
+function watchMovieResults() {
+    $("main").on("click", ".movie-result", e=> {
+        STORE.movieId = Number($(e.currentTarget).attr('id'));
+        console.log(STORE.MovieId);
+        STORE.movieTitle = $(e.currentTarget).attr('alt')
+        console.log(STORE.MovieTitle);
+        $('h4').text(`You selected ${STORE.movieTitle}, if this is correct click the Next button, if not choose a differnt movie`)
+    });
+}
+
+watchMovieResults()
+
 /*------------------ GET functions ------------------------*/
 // get food
 function getFood(searchFood){
@@ -126,7 +160,7 @@ function getFood(searchFood){
         }
         throw new Error(responseFood.statusText);
     })
-    .then((responseJsonFood)=>displayFoodResults(responseJsonFood))
+    .then((responseJsonFood)=> displayFoodResults(responseJsonFood))
     .catch((err)=>{
         $("#js-error-messageFood").text(`Something went wrong: ${err.message}`); 
     })
@@ -141,7 +175,7 @@ function getMovie(searchMovie){
         }
         throw new Error(responseMovie.statusText);
     })
-    .then((responseJsonMovie)=>console.log(responseJsonMovie))
+    .then((responseJsonMovie)=> displayMovieResults(responseJsonMovie))
     .catch((err)=>{
         $("#js-error-messageFood").text(`Something went wrong: ${err.message}`); 
     })
@@ -153,23 +187,47 @@ function displayFoodResults(responseJsonFood) {
     console.log(responseJsonFood);
   // iterate through the articles array, stopping at the length of array
   $("main").html(`
-  <h2>Choose the recipe you want then click the Next button</h2>
+  <h2>Choose the recipe you want by clicking on the picture that looks good</h2>
     <button id="food">Next</button>
-    <p class="result-id"></p>
+    <h4 class="result-id"></h4>
   `)
   for (let i = 0; i < responseJsonFood.results.length; i++) {
     // for each state in the results
     //array, add a list item to the results
     //list with the park name, url, description, and address
     $("main").append(`
-      <div>
+      <div class="food-results">
       <h3>${responseJsonFood.results[i].title}</h3>
-      <img src=${responseJsonFood.results[i].image} alt=${responseJsonFood.results[i].title} class="recipe-result" id="${responseJsonFood.results[i].id}">
+      <img src="${responseJsonFood.results[i].image}" alt="${responseJsonFood.results[i].title}" class="recipe-result" id="${responseJsonFood.results[i].id}">
       </div>`,
     );
   }
 
 }
+
+function displayMovieResults(responseJsonMovie) {
+    console.log(responseJsonMovie);
+  // iterate through the articles array, stopping at the length of array
+  $("main").html(`
+  <h2>Choose the movie you want by clicking on the movie poster</h2>
+    <button id="movie">Next</button>
+    <h4 class="result-id"></h4>
+  `)
+  for (let i = 0; i < responseJsonMovie.results.length; i++) {
+    // for each state in the results
+    //array, add a list item to the results
+    //list with the park name, url, description, and address
+    $("main").append(`
+      <div class="movie-results">
+      <h3>${responseJsonMovie.results[i].title}</h3>
+      <img src="https://image.tmdb.org/t/p/w300/${responseJsonMovie.results[i].poster_path}" alt="${responseJsonMovie.results[i].title}" class="recipe-result" id="${responseJsonMovie.results[i].id}">
+      <p>${responseJsonMovie.results[i].overview}</p>
+      </div>`,
+    );
+  }
+
+}
+
 
 /*---------------- event handlers -------------------------*/
 function onStart() {
@@ -187,6 +245,10 @@ function onRestart() {
     $("main").on('click', '#restart', restartPlan);
 }
 
+/*function onResults() {
+    $("main").on('click', '#results', results)
+}
+*/
 function startedPlan() {
     STORE.started = true;
     render();
@@ -201,28 +263,25 @@ function movieFill() {
     STORE.formMovie = true;
     render();
 }
-
-
+/*function results() {
+    STORE.results = true;
+    render();
+}
+*/
 function restartPlan() {
     STORE.started = false;
     STORE.formFood = false;
     STORE.formMovie = false;
-    STORE.food = {};
-    STORE.movie = {};
     render();
 }
 
-// display results function 
-/*function displayResults(responseJson) {
-    console.log(responseJson);
-}
-*/
 
 function dateNight() {
-    //onRestart();
+    onRestart();
     onStart();
     onFood();
     onMovie();
+    //onResults();
     render();
 }
 
